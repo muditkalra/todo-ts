7a93daa54e939fbc8b793d894d8f610dfbee75db
muditkalra <muditkalra267@gmail.com>
2024-08-30 18:48:21 +0530

    ReadMe added

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# Todo App
+
+This is a Todo app created using typescript and vite.
+The Project is created as a part of my learning process of typescript
+- Here, I used *typescript* and did **DOM** manipulation 
+
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import './style.css'
 
 
 interface Todo {
-  title: string;
-  isComplete: boolean;
-  readonly id: string;
+	title: string;
+	isComplete: boolean;
+	readonly id: string;
 }
 
 
@@ -17,63 +17,58 @@ const todoInput = document.getElementById('input') as HTMLInputElement;
 const myForm = document.getElementById('myForm') as HTMLFormElement;
 
 myForm.onsubmit = (event: SubmitEvent) => {
-  event.preventDefault();
+	event.preventDefault();
 
-  const todo: Todo = {
-    title: todoInput.value,
-    isComplete: false,
-    id: String(Math.random() * 1000)
-  }
-  todoInput.value = ''
+	const todo: Todo = {
+		title: todoInput.value,
+		isComplete: false,
+		id: String(Math.random() * 1000)
+	}
+	todoInput.value = ''
 
-  // todos.push(todo);
-  renderTodo(todo);
+	// todos.push(todo);
+	renderTodo(todo);
 }
 
 const generateTodoItem = (title: string, isComplete: boolean, id: string) => {
-  const todo: HTMLDivElement = document.createElement('div');
-  todo.className = "todo";
-  todo.id = id;
+	const todo: HTMLDivElement = document.createElement('div');
+	todo.className = "todo";
+	todo.id = id;
 
-  //creating p for title
-  const titlep: HTMLParagraphElement = document.createElement('p');
+	//creating p for title
+	const titlep: HTMLParagraphElement = document.createElement('p');
 
-  titlep.innerText = title;
+	titlep.innerText = title;
 
-  //creating a check box
-  const checkBox: HTMLInputElement = document.createElement('input');
-  checkBox.setAttribute('type', 'checkbox');
-  checkBox.className = 'isComplete';
-  checkBox.checked = isComplete;
-  checkBox.onchange = () => {
-    titlep.className = checkBox.checked ? 'textCut' : '';
-  }
+	//creating a check box
+	const checkBox: HTMLInputElement = document.createElement('input');
+	checkBox.setAttribute('type', 'checkbox');
+	checkBox.className = 'isComplete';
+	checkBox.checked = isComplete;
+	checkBox.onchange = () => {
+		titlep.className = checkBox.checked ? 'textCut' : '';
+	}
 
 
-  //creating a delete button 
+	//creating a delete button 
 
-  const btn: HTMLButtonElement = document.createElement('button');
-  btn.innerText = 'X';
-  btn.className = 'deletebtn';
-  btn.onclick = () => {
-    console.log('clicked')
-    todos.filter(item => item.id !== id);
-    console.log(todos);
-    // renderTodo(todos);
-    const div = document.getElementById(id) as HTMLDivElement;
-    todoContainer.removeChild(div);
-  }
+	const btn: HTMLButtonElement = document.createElement('button');
+	btn.innerText = 'X';
+	btn.className = 'deletebtn';
+	btn.onclick = () => {
+		todos.filter(item => item.id !== id);
+		const div = document.getElementById(id) as HTMLDivElement;
+		todoContainer.removeChild(div);
+	}
 
-  // appending all items
-  todo.append(checkBox, titlep, btn);
+	// appending all items
+	todo.append(checkBox, titlep, btn);
 
-  todoContainer.append(todo);
+	todoContainer.append(todo);
 
 }
 
 const renderTodo = (todo: Todo) => {
-  // todos.forEach((item) => {
-  generateTodoItem(todo.title, todo.isComplete, todo.id);
-  // })
-  return todos
+	generateTodoItem(todo.title, todo.isComplete, todo.id);
+	return todos
 }
